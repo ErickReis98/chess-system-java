6a5d3664e6f25ac4e4e79953eab10b9393b1b1c9
Erick Reis <erickrr98@gmail.com>
2022-05-02 21:01:12 -0300

    Adicionando possiveis movimentos de uma peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -38,6 +38,10 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("Não há peça nessa posição.");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Não há movimentos possiveis para a peça escolhida.");
+			
+		}
 	}
 	
 	private Piece makeMove(Position source, Position target) {
